74bcb05252bfbf67351820cbec7ffa039eaef264
iacarpenter <iancarpentersuhy@live.com>
2022-04-22 17:30:17 -0400

    finished data exploratin, added CustomAttributeDropper

--- diabetes_functions.py
+++ diabetes_functions.py
@@ -1,4 +1,8 @@
 from basic_functions import load_csv_data, stratified_split_dataset
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 DATASET_PATH = "./diabetes_012_health_indicators_BRFSS2015.csv"
 SEPARATOR = ","
@@ -15,3 +19,21 @@ def split_diabetes_dataset(
         diabetes_data, test_size, random_state, 
         stratification_feature, label_feature)
 
+class CustomAttributeDropper (BaseEstimator, TransformerMixin):
+
+    # transformer to drop PhysHlth, DiffWalk, and Education
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        physhlth_ix = 15
+        diffwalk_ix = 16
+        education_ix = 19
+        X = np.delete(X, [physhlth_ix, diffwalk_ix, education_ix], 1)
+        return X
+
+# pipeline to drop features, then scale
